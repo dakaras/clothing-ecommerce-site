1be256fee01eb569afb3d384287711493eb32eeb
dakaras <dan.karagiannis@protonmail.ch>
2021-01-25 19:50:23 -0600

    imported material-ui

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+import {useQuery} from 'react-query'
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingCart from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+import {Wrapper} from './App.styles'
+
+const App =() => {
+    return 
+    <div className= 'App'>
+      Start
+    </div>
+}
+
+export default App;
